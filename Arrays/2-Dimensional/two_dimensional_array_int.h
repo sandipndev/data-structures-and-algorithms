551dcb9805685d47c9b0772f7511e98b71b4e7f0
Sandipan Dey <sandipandey.12@gmail.com>
2019-08-24 11:36:42 +0530

    2d header

--- Arrays/2-Dimensional/two_dimensional_array_int.h
+++ Arrays/2-Dimensional/two_dimensional_array_int.h
@@ -12,3 +12,54 @@ void matrix_user_input(matrix);
 void print_matrix(matrix);
 void free_matrix(matrix);
 
+matrix create_matrix(int rows, int cols) {
+    matrix tx;
+    
+    // For the matrix data
+    tx.base = (int **) calloc(rows, sizeof(int *));
+    int i;
+    for (i=0; i<rows; i++) {
+        tx.base[i] = (int *) calloc(cols, sizeof(int));
+    }
+
+    // And some metadata
+    tx.cols = cols;
+    tx.rows = rows;
+
+    return tx;
+}
+
+void matrix_user_input(matrix rx) {
+    fflush(stdin);
+    int i, j;
+    for (i=0; i<rx.rows; i++) {
+        for (j=0; j<rx.cols; j++) {
+            printf("?(%d,%d): ", i+1, j+1);
+            scanf("%d", &rx.base[i][j]);
+        }
+    }
+}
+
+void print_matrix(matrix rx) {
+    int i, j;
+    for (i=0; i<rx.rows; i++) {
+        if(i==0)    printf("[  [");
+        else        printf("   [");
+        for (j=0; j<rx.cols; j++) {
+            printf(" %4d,", rx.base[i][j]);
+        }
+        if (j==0)   printf("]");
+        else        printf("\b]");
+        if (i==rx.rows-1)   printf("  ");
+        else                printf("\n");
+    }
+    printf("]\n");
+}
+
+void free_matrix(matrix rx) {
+    int i;
+    for (i=0; i<rx.rows; i++) {
+        free(rx.base[i]);
+    }
+    free(rx.base);
+}
