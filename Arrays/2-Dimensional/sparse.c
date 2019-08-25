d02a2cd70c43e41aa6bb3bcbd32617a326821c40
Sandipan Dey <sandipandey.12@gmail.com>
2019-08-25 12:48:22 +0530

    sparse half added

---
+++ Arrays/2-Dimensional/sparse.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "two_dimensional_array_int.h"
+
+int check_sparse(matrix);
+matrix sparse_matrix_three_tuple(matrix);
+matrix recreate_from_sparse_three_tuple(matrix);
+
+int main() {
+    int r, c;
+    printf("?Rows: ");
+    scanf("%d", &r);
+    printf("?Cols: ");
+    scanf("%d", &c);
+
+    return 0;
+}
+
+int check_sparse(matrix rx) {
+    int i, j, n_zeros=0;
+    for (i=0; i<rx.rows; i++) {
+        for (j=0; j<rx.cols; j++) {
+            if(rx.base[i][j] == 0) {
+                n_zeros++;
+            }
+        }
+    }
+
+    if ((float) n_zeros >= 2.0 / 3.0 * ((float) rx.rows * rx.cols))
+        return n_zeros;
+    else
+        return -1;
+}
+
+matrix sparse_matrix_three_tuple(matrix rx) {
+    int n_zeros = check_sparse(rx);
+    if (n_zeros == -1) {
+        n_zeros = 0;
+    }
+    int n_nonzeros = (rx.rows * rx.cols) - n_zeros;
+    matrix sparse = create_matrix(n_nonzeros + 1, 3);
+
+    sparse.base[0][0] = rx.rows;
+    sparse.base[0][1] = rx.cols;
+    sparse.base[0][2] = n_nonzeros;
+
+    int i, j, c=1;
+
+    for (i=0; i<rx.rows; i++) {
+        for (j=0; j<rx.cols; j++) {
+            if (rx.base[i][j] != 0) {
+                sparse.base[c][0] = i;
+                sparse.base[c][1] = j;
+                sparse.base[c][2] = rx.base[i][j];
+                c++;
+            }
+        }
+    }
+
+    return sparse;
+}
+
+matrix recreate_from_sparse_three_tuple(matrix rx) {
+    int req_rows = rx.base[0][0];
+    int req_cols = rx.base[0][1];
+    int n_nz_eles = rx.base[0][2];
+
+    matrix original = create_matrix(req_rows, req_cols);
+    int i, row, col;
+
+    for (i=1; i<=n_nz_eles; i++) {
+        row = rx.base[i][0];
+        col = rx.base[i][1];
+        original.base[row][col] = rx.base[i][2];
+    }
+
+    return original;
+}
