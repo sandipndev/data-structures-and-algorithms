f78026a7bc92109f379a03e67404ad82b13f7064
Sandipan Dey <sandipandey.12@gmail.com>
2019-09-22 13:23:52 +0530

    linked list menu driven with sort non working

---
+++ Linked List/linked_list.h
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+	int data;
+	struct node *link;
+} node;
+
+node *create_node(int key) {
+	node *temp;
+	temp = (node *) calloc(1, sizeof(node));
+	if (temp == NULL) {
+		printf("[OUT_OF_MEM] Memory Error\n");
+		exit(0);
+	}
+	temp -> data = key;
+	temp -> link = NULL;
+	return temp;
+}
+
+void display_list(node *head) {
+	printf("[ ");
+	while (head!=NULL) {
+		printf("%d,", head->data);
+		head = head->link;
+	}
+	printf("\b]\n");
+}
+
+void display_list_with_mem(node *head) {
+	printf("[ ");
+	while (head!=NULL) {
+		printf("<%u> -> %d,", head, head->data);
+		head = head->link;
+	}
+	printf("\b]\n");
+}
+
+node *insert_first(node *head, int key) {
+	node *temp;
+	temp = create_node(key);
+	if (head == NULL) {
+		head = temp;
+	} else {
+		temp->link = head;
+		head = temp;
+	}
+	return head;
+}
+
+node *insert_last(node *head, int key) {
+	node *temp, *p = head;
+	temp = create_node(key);
+	if (head == NULL) {
+		head = temp;
+	} else {
+		while ((head->link)!=NULL) {
+			head = head->link;
+		}
+		head->link = temp;
+	}
+	return p;
+}
+
+node *insert_before(node *head, int data, int key) {
+	node *temp, *p;
+	if (head == NULL) {
+		printf("[!] Empty List.\n");
+		return head;
+	}
+	p = head;
+	while (p->link!=NULL) {
+		if((p->link)->data == data)
+			break;
+		p = p->link;
+	}
+	if (p->link == NULL)
+		printf("[X] Data Not Found.\n");
+	else {
+		temp = create_node(key);
+		temp->link = p->link;
+		p->link = temp;
+	}
+	return head;
+}
+
+node *insert_after(node *head, int data, int key) {
+	node *temp, *p;
+	if (head == NULL) {
+		printf("[!] Empty List. \n");
+		return head;
+	}
+	p = head;
+	while(p!=NULL) {
+		if (p->data == data)
+			break;
+		p = p->link;
+	}
+	if (p==NULL) {
+		printf("[X] Data Not Found.\n");
+	} else {
+		temp = create_node(key);
+		temp->link = p->link;
+		p->link = temp;
+	}
+	return head;
+}
+
+node *delete_first(node *head) {
+	if (head == NULL) {
+		printf("[LLIST_UFLOW] Linked List was Underflow!\n");
+		return head;
+	}
+	node *temp = head;
+	head = head -> link;
+	printf("Deleted %d.\n", temp->data);
+	free(temp);
+	return head;
+}
+
+node *delete_last(node *head) {
+	if (head == NULL) {
+		printf("[LLIST_UFLOW] Linked List was Underflow!\n");
+		return head;
+	}
+	// If one element
+	if (head->link == NULL) {
+		printf("Deleted %d.\n", head->data);
+		free(head);
+		return NULL;
+	}
+	node *temp = head, *p = head;
+	head = head->link;
+	while (head->link != NULL) {
+		temp = head;
+		head = head->link;
+	}
+	temp->link = NULL;
+	printf("Deleted %d.\n", head->data);
+	free(head);
+	return p;
+}
+node *delete_any(node *head, int key) {
+	node *p, *q;
+	if(head == NULL) {
+		printf("[!] Empty List. \n" );
+		return head;
+	}
+	if (head -> data == key) {
+		p = head;
+		head = head -> link;
+		free(p);
+	} else {
+		p = head;
+		while (p!= NULL && p->data != key) {
+			q = p;
+			p = p->link;
+		}
+		if (p == NULL)
+			printf("[X] Key Not Found.\n");
+		else {
+			q->link = p->link;
+			free(p);
+		}
+	}
+	return head;
+}
+
+node *insert_elem_sorted(node *head, int key) {
+	node *p, *q, *temp = create_node(key);
+	if (head == NULL) {
+		head = temp;
+		return head;
+	}
+	if (head->data >= key) {
+		temp->link = head;
+		head = temp;
+	} else {
+		p = head;
+		while (p!=NULL && p->data <= key) {
+			q = p;
+			p = p->link;
+		}
+		q->link = temp;
+		temp->link = p;
+	}
+	return head;
+}
+
+node *reverse(node *head) {
+	if (head == NULL) {
+		return head;
+	}
+	node *rev = NULL;
+	while (head != NULL) {
+		rev = insert_first(rev, head->data);
+		head = head->link;
+	}
+	return rev;
+}
+
+node *sort(node *head) {
+	node *p, *q, *m;
+	p = head;
+	int temp;
+	while ((p->link) != NULL) {
+		m = p;
+		q = p->link;
+		while(q!=NULL){
+			if(q->data < m->data) {
+				m = q;
+			}
+			q = q->link;
+		}
+		if (m!=p) {
+			temp = p->data;
+			p->data = q->data;
+			q->data = temp;
+		}
+		p = p->link;
+	}
+	return head;
+}
+
---
+++ Linked List/menu_driven_llist.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+
+int main() {
+	node *head = NULL;
+	int choice, key, pdata;
+
+	printf("[ THE CELEBRATED LINKED LIST MENU DRIVEN PROGRAM ]\n");
+	printf("These are your options :-\n");
+	printf("1. Display list\n");
+	printf("2. Display list along with mem addr\n");
+	printf("3. Insert element at first\n");
+	printf("4. Insert element at last\n");
+	printf("5. Delete element from first\n");
+	printf("6. Delete element from last\n");
+	printf("7. Insert element before another element\n");
+	printf("8. Insert element after another element\n");
+	printf("9. Delete any element\n");
+	printf("10. Reverse Linked List\n");
+	printf("11. Insert Element in Sorted Manner\n");
+	// printf("12. Sort\n");
+
+
+	while (1) {
+		fflush(stdin);
+		printf("? Your choice: ");
+		scanf("%d", &choice);
+
+		switch(choice) {
+			case 1: display_list(head);
+			break;
+			case 2: display_list_with_mem(head);
+			break;
+			case 3: printf("? Term to enter: ");
+				scanf("%d", &key);
+				head = insert_first(head, key);
+			break;
+			case 4: printf("? Term to enter: ");
+				scanf("%d", &key);
+				head = insert_last(head, key);
+			break;
+			case 5: head = delete_first(head);
+			break;
+			case 6: head = delete_last(head);
+			break;
+			case 7: printf("? Data before which element needs to be added: ");
+				scanf("%d", &pdata);
+				printf("? Data to be added: ");
+				scanf("%d", &key);
+				head = insert_before(head, pdata, key);
+			break;
+			case 8: printf("? Data after which element needs to be added: ");
+				scanf("%d", &pdata);
+				printf("? Data to be added: ");
+				scanf("%d", &key);
+				head = insert_after(head, pdata, key);
+			break;
+			case 9: printf("? Element to delete: ");
+				scanf("%d", &key);
+				head = delete_any(head, key);
+			break;
+			case 10: printf("Reversed: ");
+				display_list(reverse(head));
+			break;
+			case 11: printf("? Term to enter: ");
+				scanf("%d", &key);
+				head = insert_elem_sorted(head, key);
+			break;
+			// case 12: printf("Sorted List: ");
+			// 	display_list(sort(head));
+			// break;
+			default: 
+				printf("Goodbye!\n");
+				free(head);
+				exit(0);
+		}
+	}
+}
